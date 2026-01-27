c7a76af3f44b8cde23371844a8c31a493a1eb985
Diego Alejandro Diaz <90485793+AlejandroDeveloper2@users.noreply.github.com>
2026-01-27 16:24:26 -0500

    Optimización de rendimiento

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,25 @@
 import { defineConfig } from "vite";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 export default defineConfig({
   publicDir: "public",
   build: {
     outDir: "dist",
     assetsDir: "assets",
+    minify: "terser",
+    terserOptions: {
+      compress: {
+        drop_console: true,
+        drop_debugger: true,
+      },
+    },
+    rollupOptions: {},
   },
+  plugins: [
+    ViteImageOptimizer({
+      png: { quality: 80 },
+      jpeg: { quality: 85 },
+      webp: { quality: 85 },
+    }),
+  ],
 });
